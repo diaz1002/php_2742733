c65a6aaf895ae8fa6cfb873a961abda06f181e48
C4PTUR4 <captura669@gmail.com>
2024-05-10 16:48:42 -0500

    feat: ejercicios terminados de variables

--- 02-condicionales/index.php
+++ 02-condicionales/index.php
@@ -44,6 +44,10 @@ if($años < 18) {
     echo "no puede entrar al VIP <br/>";
 }
 
+{
+    echo "<br/> <br/> <br/>"; 
+}
+
 
 ?>
 
@@ -57,4 +61,52 @@ $old = "16";
 
 $velocidad = "30";
 
-$statura = "20";
+$estatura = "174";
+
+if ($velocidad > 27 && $estatura > 170) {
+    echo "puede ingresar al equipo <br/>";
+} else {
+    echo "no puede ingresar <br/>";
+}
+
+if ($velocidad > 27 && $estatura > 170 && $old > 18) {
+    echo "vas a ligas mayores";
+} else {
+    echo "vas a ligas menores <br/> <br/> ";
+}
+
+?>
+
+
+<!-- ejercicio 4 -->
+
+<?php
+$aire = "rojo";
+
+switch($aire){
+    case "verde":
+        echo "calidad del aire buena";
+    break;
+    case "amarillo":
+        echo "calidad del aire moderado";
+    break;
+    case "naranja":
+        echo "calidad del aire no es saludable para grupos sensibles";
+    break;
+    case "rojo":
+        echo "calidad del aire no es saludable";
+    break;
+    case "purpura":
+        echo "calidad del aire es muy poco saludable";
+    break;
+    case "marron":
+        echo "calidad del aire peligrosa";
+    break;
+    default:
+        echo "el color no es válido";
+    break;
+}
+
+
+
+?>
