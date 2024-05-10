f61e5827ceeeb4fca8bc241ee5dc746e61750e7b
C4PTUR4 <captura669@gmail.com>
2024-05-10 14:57:52 -0500

    v2

---
+++ 02-condicionales/index.php
@@ -0,0 +1,42 @@
+<!-- ejercicio 1 -->
+
+<?php
+
+$cantidad = "40";
+
+?>
+
+<?php
+
+if($cantidad < 12)   {
+    echo "caja rapida";
+} else{
+    echo "caja normal";
+}
+
+echo "<br/> <br/>";
+?>
+
+
+
+<?php
+
+$edad = "20";
+
+?>
+
+<?php
+
+if($edad >= 18) {
+    echo "puede votar";
+} else{
+    echo "no puede votar";
+}
+
+?>
+
+
+<!--  jercicio 2 -->
+
+
+
